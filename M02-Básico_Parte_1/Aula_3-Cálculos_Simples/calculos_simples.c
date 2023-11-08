4ca4e735b2a8a87d7ca0ac0b3c5f63499e39004a
wellington <wellington281482@gmail.com>
2023-11-07 22:57:08 -0300

    Reorganização de código

---
+++ M02-Básico_Parte_1/Aula_3-Cálculos_Simples/calculos_simples.c
@@ -0,0 +1,31 @@
+/* =================================================================================================
+	
+	Descrição: Cálculos simples
+	Autor: Wellington
+	Data: 07/11/2023
+	Atualização: --
+	
+================================================================================================== */
+
+
+/* Blibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Função Principal */
+int main(int argc, char *argv[]) {
+	
+	printf("Resultado = %d\n\n", 8-1);				/* Realiza uma subtração */
+	printf("Resultado = %d\n\n", 10+4);				/* Realiza uma soma */
+	printf("Resultado = %d\n\n", 12/6);				/* Realiza uma divisão */
+	printf("Resultado = %d\n\n", 2*11);				/* Realiza uma soma */
+	
+	printf("Resultado = %d\n\n", 16+8/2);	
+	printf("Resultado = %d\n\n", (16+8)/2);	
+	system("PAUSE");						/* Faz uma parada na execução do programa */
+		
+	return 0;							/* Retorna '0' se tudo  ocorrer bem na execução */
+	
+}  /* end main */
+

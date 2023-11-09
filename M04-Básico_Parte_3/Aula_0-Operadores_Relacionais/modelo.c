2a84faeac6b2ae91fc557174aeb69a745b46c90e
wellington <wellington281482@gmail.com>
2023-11-09 00:19:33 -0300

    Incluída biblioteca

---
+++ M04-Básico_Parte_3/Aula_0-Operadores_Relacionais/modelo.c
@@ -0,0 +1,29 @@
+
+/* =================================================================================================
+	Módulo , Parte 
+	Aula 
+	Descrição: 
+	Autor: Wellington
+	Data: 
+	Atualização: --
+================================================================================================== */
+
+/* Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+
+/* Função Principal */
+int main(int argc, char *argv[]) {
+
+    printf("----------------------------------------------\n");	        /* Cabeçalho */
+    printf("Teste de acentuação\n");
+    printf("----------------------------------------------\n");	
+    
+    printf("----------------------------------------------\n");	        /* Rodapé */
+    
+	/* system("PAUSE"); */					        /* Faz uma parada na execução do programa */
+		
+	return 0;							/* Retorna '0' se tudo ocorrer bem na execução */
+	
+}  /* end main */

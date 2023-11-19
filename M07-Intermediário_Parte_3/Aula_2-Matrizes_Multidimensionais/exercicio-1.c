854fd8a3b30583d3c806b8dc7042c544fb799146
wellington <wellington281482@gmail.com>
2023-11-19 15:10:36 -0300

    Exercício

--- M07-Intermediário_Parte_3/Aula_2-Matrizes_Multidimensionais/exercicio-1.c
+++ M07-Intermediário_Parte_3/Aula_2-Matrizes_Multidimensionais/exercicio-1.c
@@ -14,41 +14,95 @@
 #include <stdlib.h>
 #include <locale.h> 
 
-#define LINHA 4
-#define COLUNA 6
+#define HEIGHT 4
+#define WIDTH 6
+
+
+void draw_grid(unsigned int mat[HEIGHT][WIDTH], int height, int width);
+
 
 /* Função Principal */
 int main(int argc, char *argv[])
 {
 									
     /* setlocale(LC_CTYPE, "Portuguese"); */ 
-
-    int mat[3][4] = { { 15, 22,  9,  1},
-                      { 65, 42,  0, 71},
-                      {  7, 29, 33, 62} };;
-
-    register int i,j,col=0;
     
-    printf("-----------------------\n");            
-    for(i=0;i<3;i++)
-    {
-        printf("|");
-        for(j=0;j<4;j++){
-            printf(" %3d ", mat[i][j]);
-            printf("|");
-        }
-        printf(" |");
+    unsigned int mat[HEIGHT][WIDTH] = {{0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0}}, number;
+    unsigned char line, column;
+    
+
+    draw_grid(mat, HEIGHT, WIDTH);  
+    while(1) 
+    {    
         putchar('\n');
-        printf("-----------------------\n");            
-    }
-    putchar('\n'); 
-     
+
+        printf(">>> Linha : ");
+        scanf("%hhd", &line);
     
-    
-    
-    
+        printf(">>> Coluna: ");
+        scanf("%hhd", &column);
+        
+        printf(">>> Valor : ");
+        scanf("%d", &number);
+
+        if(number < 0 || number > 999)
+        { 
+           puts("\n - Valor fora da faixa!");
+ 
+        } /* end if */
+        else
+        {
+            if(!(line > HEIGHT || column > WIDTH))
+                mat[line - 1][column - 1] = number;
+
+            draw_grid(mat, HEIGHT, WIDTH);
+
+        } /* end else */
+
+    } /* end while */
+              
     /* system("PAUSE");               							/* Faz uma parada na execução do programa */
 		
     return 0;		        							/* Retorna '0' se tudo ocorrer bem na execução */
 	
 }  /* end main */
+
+
+void draw_grid(unsigned int mat[HEIGHT][WIDTH], int heigth, int width)
+{
+    register int i, line, column;
+    
+    system("clear");
+
+    puts("---------------------------------------------------------------");
+    printf("*                     TABELA  %d x %d                           *\n", HEIGHT, WIDTH);
+    puts("---------------------------------------------------------------");
+    
+    for(i=0; i < width; i++)
+        if(line == 0) printf("       C%d", i + 1);
+    putchar('\n');
+    
+    for(line=0; line < heigth; line++)
+    {
+        puts("    -------------------------------------------------------");            
+        printf(" L%d", line + 1);
+        for(column=0; column < width; column++){
+            if(column < width) printf(" |");
+            printf("   %3d ", mat[line][column]);
+
+        } /* end for internal */
+        puts(" |");
+
+    } /* end for external */
+
+    puts("    -------------------------------------------------------");            
+
+    puts("---------------------------------------------------------------");
+    puts("Informe valores na faixa de 0 a 999 (00 para sair)");
+
+} /* end draw_grade */
+
+

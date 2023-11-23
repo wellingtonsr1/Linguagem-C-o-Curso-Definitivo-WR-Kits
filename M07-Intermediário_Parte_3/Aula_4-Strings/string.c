96f6bd817e40bb3761bdc229e30a9f48484f4703
wellingtonrodrigues <wellington281482@gmail.com>
2023-11-23 07:49:22 -0300

    string alt

--- M07-Intermediário_Parte_3/Aula_4-Strings/string.c
+++ M07-Intermediário_Parte_3/Aula_4-Strings/string.c
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h> 
-
+#include <string.h>
 
 #define DIM 10
 /*================================================================================================== */

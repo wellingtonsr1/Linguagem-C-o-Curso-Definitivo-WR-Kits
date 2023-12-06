b491449ce998a25fbe0b10b75e492e20ac81254c
wellingtonrodrigues <wellington281482@gmail.com>
2023-12-06 09:39:54 -0300

    arquivos modificados

--- M08-Avançado_Parte_1/Aula_5-Passagem_de_Parâmetros_e_Recursividade/exercicio-1.c
+++ M08-Avançado_Parte_1/Aula_5-Passagem_de_Parâmetros_e_Recursividade/exercicio-1.c
@@ -1,6 +1,6 @@
 /* =================================================================================================
 	Módulo 8
-	Aula 3
+	Aula 5
 	Descrição: Exercício 1
 	Autor: Wellington
 	Data: 05/12/2023
@@ -11,7 +11,9 @@
 /* ----- Bibliotecas ----- */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 #include <locale.h> 
+
 
 /* ================================================================================================= */
 /* ----- Função Principal ----- */
@@ -24,19 +26,27 @@ int main(int argc, char *argv[])
     printf("----------------------------------------------\n");	                        /* Cabeçaalho */
     printf("%25s\n", "Exercicio 1");   
     printf("----------------------------------------------\n");	
+    putchar('\n');
+     
+    if(argc == 1)
+    	printf("Uso: programa.exe ohm valor_1 valor2 valor_3 valor_4...valor_x valor_y\n", argv[0]);
+    else
+    {
+    	printf(" Resistores: ");
+    	for(i=2; i < argc; i+=2)
+   		{  
+        	printf("%.2f", atof(argv[i]) / atof(argv[i+1]));
+        	if(i >= 2 && i < argc - 2) printf(" | ");
+    	}
+    	printf(" Ohms\n");
+    	
+	} /* end esle */
+	
+	putchar('\n');
+	printf("----------------------------------------------\n");         	              /* Rodapé */
+    putchar('\n');
     
-    printf(" Resistores: ");
-    for(i=2; i < argc; i+=2)
-    {  
-        printf("%.2f", atof(argv[i]) / atof(argv[i+1]));
-        if(i >= 2 && i < argc - 2) printf(" | ");
-    }
-    printf(" Ohms\n");
-
-    printf("----------------------------------------------\n");         	              /* Rodapé */
-    
-    system("PAUSE");
-
+	system("PAUSE");
     return 0;		        							                              /* Retorna '0' se tudo ocorrer bem na execução */
 	
 }  /* end main */
